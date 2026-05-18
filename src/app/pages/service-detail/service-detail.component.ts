f9fcd7156335fc1addb786074b05be14689155d3
gambbon <sergiosvalero@gmail.com>
2026-05-18 09:17:40 +0200

    He añadido videos embebidos en las pestañas que cuenten con video, para que el video sea el centro de atención

--- src/app/pages/service-detail/service-detail.component.ts
+++ src/app/pages/service-detail/service-detail.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { HomelabDataService, Service } from '../../services/homelab-data.service';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-service-detail',
@@ -13,16 +14,38 @@ import { NavbarComponent } from '../../components/navbar/navbar.component';
 })
 export class ServiceDetailComponent implements OnInit {
   service?: Service;
+  embedUrl?: SafeResourceUrl;
 
   constructor(
     private route: ActivatedRoute,
-    private data: HomelabDataService
+    private data: HomelabDataService,
+    private sanitizer: DomSanitizer
   ) {}
 
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get('id');
-    if (id) {
-      this.service = this.data.getServiceById(id);
+    this.service = this.data.getServiceById(id ?? '');
+ 
+    if (this.service?.videoUrl) {
+      this.embedUrl = this.sanitizer.bypassSecurityTrustResourceUrl(
+        this.toEmbedUrl(this.service.videoUrl)
+      );
     }
+  }
+
+  // Convertir una URL de Youtube en URL embebible.
+   private toEmbedUrl(url: string): string {
+    const ytMatch = url.match(
+      /(?:youtube\.com\/(?:watch\?v=|shorts\/)|youtu\.be\/)([a-zA-Z0-9_-]{11})/
+    );
+    if (ytMatch) {
+      return `https://www.youtube.com/embed/${ytMatch[1]}?rel=0&modestbranding=1`;
+    }
+    const vimeoMatch = url.match(/vimeo\.com\/(\d+)/);
+    if (vimeoMatch) {
+      return `https://player.vimeo.com/video/${vimeoMatch[1]}`;
+    }
+ 
+    return url;
   }
 }
